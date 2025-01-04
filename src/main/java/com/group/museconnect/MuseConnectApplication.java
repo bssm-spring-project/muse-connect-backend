a5e9fa8b7d2749d9e5b33e797effb0d06b958bb3
cabbage16 <qocn16@gmail.com>
2025-01-04 17:13:50 +0900

    Initial Commit

---
+++ src/main/java/com/group/museconnect/MuseConnectApplication.java
@@ -0,0 +1,13 @@
+package com.group.museconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseConnectApplication.class, args);
+    }
+
+}
